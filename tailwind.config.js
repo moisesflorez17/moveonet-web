408abb59376c501a84549e1a95438fb6806232fe
moisesflorez17 <moises.florez17@gmail.com>
2025-09-11 00:31:31 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // escanea todos los archivos dentro de src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};nfig
+};
